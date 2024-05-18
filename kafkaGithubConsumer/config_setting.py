cce8911421715410835883cbde41c5d83ede2caf
jgwentworth92 <alphabetboi92@protonmail.com>
2024-05-18 00:14:09 -0400

    adding first microservice with tests

--- kafkaGithubConsumer/config_setting.py
+++ kafkaGithubConsumer/config_setting.py
@@ -2,7 +2,7 @@ import os
 import json
 from functools import lru_cache
 from pydantic_settings import BaseSettings
-
+from typing import Optional, Dict
 
 class Settings(BaseSettings):
     """Base Configuration Class"""
@@ -10,33 +10,29 @@ class Settings(BaseSettings):
     TITLE: str = "Kafka Consumer Vector DB ingestors"
     DESCRIPTION: str = "Service consuming Kafka topics and adding to vector db."
     GITHUB_TOKEN: str
-    BROKERS: str
-    INPUT_TOPIC: str
-    OUTPUT_TOPIC: str
-    CONSUMER_CONFIG: dict[str, str]
-    PRODUCER_CONFIG: dict[str, str]
+    BROKERS: Optional[str] = None
+    INPUT_TOPIC: Optional[str] = None
+    OUTPUT_TOPIC: Optional[str] = None
+    CONSUMER_CONFIG: Optional[Dict[str, str]] = None
+    PRODUCER_CONFIG: Optional[Dict[str, str]] = None
 
     class Config:
         env_file = ".env"
         env_file_encoding = 'utf-8'
 
     @classmethod
-    def parse_env_var(cls, value: str) -> dict:
+    def parse_env_var(cls, value: str) -> Dict[str, str]:
         return json.loads(value)
-
 
 class ProductionConfig(Settings):
     pass
 
-
 class DevelopmentConfig(Settings):
     DEBUG: bool = True
-
 
 class TestingConfig(Settings):
     TESTING: bool = True
     DEBUG: bool = True
-
 
 @lru_cache()
 def get_config() -> Settings:
@@ -50,6 +46,5 @@ def get_config() -> Settings:
         return ProductionConfig()
     else:
         raise ValueError(f"Invalid environment: {environment}")
-
 
 config = get_config()
