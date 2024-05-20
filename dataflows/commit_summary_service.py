e5250abe0cef983235fc1a519eab2f267dba5b4e
jgwentworth92 <alphabetboi92@protonmail.com>
2024-05-20 16:20:59 -0400

    dataflow for second microservice

---
+++ dataflows/commit_summary_service.py
@@ -0,0 +1,44 @@
+from bytewax.dataflow import Dataflow
+import bytewax.operators as op
+from bytewax.connectors.kafka import KafkaSource, KafkaSink, KafkaSinkMessage
+from confluent_kafka import OFFSET_END
+from config.config_setting import config
+from services.message_processing_service import process_message
+from utils.kafka_utils import inspect_output_topic
+import orjson
+
+# Application setup
+brokers = [config.BROKERS]
+input_topic = config.OUTPUT_TOPIC
+output_topic = config.PROCESSED_TOPIC
+consumer_config = config.CONSUMER_CONFIG
+producer_config = config.PRODUCER_CONFIG
+
+# Bytewax dataflow setup
+flow = Dataflow("commit_summary_service")
+
+# Create KafkaSource for consuming messages from Kafka
+kafka_input = op.input("kafka-in", flow,
+                       KafkaSource(brokers=brokers, starting_offset=OFFSET_END, topics=[input_topic],
+                                   add_config=consumer_config))
+
+# Process each message
+processed_messages = op.flat_map("process_message", kafka_input,
+                                 lambda msg: process_message(orjson.loads(msg.value)))
+
+# Serialize processed documents
+serialized_messages = op.map("serialize_messages", processed_messages, orjson.dumps)
+
+# Create KafkaSinkMessages for each serialized document
+kafka_messages = op.map("create_kafka_messages", serialized_messages, lambda x: KafkaSinkMessage(None, x))
+
+# Output serialized messages to Kafka
+op.output("kafka-output", kafka_messages, KafkaSink(brokers=brokers, topic=output_topic, add_config=producer_config))
+
+# Input from Kafka to inspect output topic messages
+kafka_output_input = op.input("kafka-output-input", flow,
+                              KafkaSource(brokers=brokers, starting_offset=OFFSET_END, topics=[output_topic],
+                                          add_config=consumer_config))
+
+# Inspect the output topic messages
+op.inspect("inspect_output_topic", kafka_output_input, inspect_output_topic)
