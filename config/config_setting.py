197f515119c8011c349130a22d2d8d31465417cc
jgwentworth92 <alphabetboi92@protonmail.com>
2024-05-20 16:22:38 -0400

    moved config to config folder

--- config/config_setting.py
+++ config/config_setting.py
@@ -1,24 +1,26 @@
 import os
 import json
 from functools import lru_cache
-from pydantic_settings import BaseSettings
+from pydantic import  ConfigDict
 from typing import Optional, Dict
-
+from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     """Base Configuration Class"""
     APP_NAME: str = "Kafka Consumer with vector data base additions"
     TITLE: str = "Kafka Consumer Vector DB ingestors"
     DESCRIPTION: str = "Service consuming Kafka topics and adding to vector db."
-    GITHUB_TOKEN: str
+    GITHUB_TOKEN: Optional[str]
     BROKERS: Optional[str] = None
     INPUT_TOPIC: Optional[str] = None
     OUTPUT_TOPIC: Optional[str] = None
     CONSUMER_CONFIG: Optional[Dict[str, str]] = None
     PRODUCER_CONFIG: Optional[Dict[str, str]] = None
+    MODEL_PROVIDER: Optional[str] = "fake"
+    TEMPLATE: Optional[str] = None
+    OPENAI_API_KEY: Optional[str] = None
+    PROCESSED_TOPIC: Optional[str] = None
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = 'utf-8'
+    model_config = ConfigDict(env_file=".env", env_file_encoding='utf-8')
 
     @classmethod
     def parse_env_var(cls, value: str) -> Dict[str, str]:
