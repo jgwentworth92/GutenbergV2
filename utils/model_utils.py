965de21c4f55dd27ee454854a1139e7eef0469ef
jgwentworth92 <alphabetboi92@protonmail.com>
2024-05-20 16:19:43 -0400

    added utils for model setup

---
+++ utils/model_utils.py
@@ -0,0 +1,32 @@
+from typing import List
+
+from langchain.chains.base import Chain
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_community.chat_models.fake import FakeMessagesListChatModel
+from langchain_core.messages import BaseMessage
+from config.config_setting import config
+from icecream import ic
+
+def get_openai_chat_model():
+    chat_model = ChatOpenAI(temperature=0, openai_api_key=config.OPENAI_API_KEY, model="gpt-3.5-turbo")
+    return chat_model
+
+def get_fake_chat_model():
+    fake_responses: List[BaseMessage] = [BaseMessage(content="This is a fake response.", type="text")]
+    chat_model = FakeMessagesListChatModel(responses=fake_responses)
+    return chat_model
+
+def setup_chat_model():
+    if config.MODEL_PROVIDER == 'openai':
+        ic("open ai model is being used")
+        model_setup_function = get_openai_chat_model
+    elif config.MODEL_PROVIDER == 'fake':
+        ic("Fake model is being used")
+        model_setup_function = get_fake_chat_model
+    else:
+        raise ValueError(f"Unsupported chat model provider: {config.MODEL_PROVIDER}")
+    chat_model = model_setup_function()
+    prompt = ChatPromptTemplate.from_template(config.TEMPLATE)
+    return prompt | chat_model | StrOutputParser()
