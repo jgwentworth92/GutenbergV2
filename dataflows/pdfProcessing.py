86a638ce93c5ffeaeb728a1ed4de9f1273f349c7
jgwentworth92 <alphabetboi92@protonmail.com>
2024-06-13 19:39:25 -0400

    removed unused serialized step in dataflow

--- dataflows/pdfProcessing.py
+++ dataflows/pdfProcessing.py
@@ -28,12 +28,9 @@ kafka_input = op.input("kafka-in", flow,
 processed_docs = op.flat_map("pdf processing", kafka_input,
                                 lambda msg: process_pdf(orjson.loads(msg.value)))
 
-# Serialize each document
-serialized_docs = op.map("serialize_docs", processed_docs, orjson.dumps)
 
-# Key the stream
 keyed_docs = op.key_on("add_key", processed_docs, lambda doc: "pdf_key")
-batched_docs = op.collect("collect_docs", keyed_docs, timeout=timedelta(seconds=1), max_size=100)
+batched_docs = op.collect("collect_docs", keyed_docs, timeout=timedelta(seconds=1), max_size=50)
 
 # Remove the key from the batched documents
 keyless_docs = op.map("remove_key", batched_docs, lambda x: (None, x[1]))
