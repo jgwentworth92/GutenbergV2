0ad16fce30146a17c88a8a4e8b3baa4e85ddff8a
jgwentworth92 <alphabetboi92@protonmail.com>
2024-06-13 20:20:12 -0400

    batches yielded returns from processing functions

--- dataflows/github_commit_processing.py
+++ dataflows/github_commit_processing.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from bytewax.dataflow import Dataflow
 import bytewax.operators as op
 from bytewax.connectors.kafka import KafkaSource, KafkaSink, KafkaSinkMessage
@@ -29,11 +31,13 @@ kafka_input = op.input("kafka-in", flow,
 processed_commits = op.flat_map("fetch_and_emit_commits", kafka_input,
                                 lambda msg: fetch_and_emit_commits(orjson.loads(msg.value)))
 
-# Serialize each commit data
-serialized_commits = op.map("serialize_commits", processed_commits, orjson.dumps)
+
 
 # Create KafkaSinkMessages for each serialized commit data
-kafka_messages = op.map("create_kafka_messages", serialized_commits, lambda x: KafkaSinkMessage(None, x))
+keyed_docs = op.key_on("add_key",processed_commits, lambda doc: "temp_key")
+batched_docs = op.collect("collect_docs", keyed_docs, timeout=timedelta(seconds=1), max_size=50)
+keyless_docs = op.map("remove_key", batched_docs, lambda x: (None, x[1]))
+kafka_messages = op.map("create_kafka_messages", keyless_docs, lambda x: KafkaSinkMessage(None, orjson.dumps(x[1])))
 
 # Output serialized data to Kafka
 op.output("kafka-vector-raw-add", kafka_messages, KafkaSink(brokers=brokers, topic=output_topic, add_config=producer_config,)) # to vctor db
