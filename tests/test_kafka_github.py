9194bfbfe825ff627fe531448ae282c9c7091232
jgwentworth92 <alphabetboi92@protonmail.com>
2024-06-19 21:06:21 -0400

    added log for for first topic

--- tests/test_kafka_github.py
+++ tests/test_kafka_github.py
@@ -22,6 +22,14 @@ def test_kafka_integration(produce_messages,kafka_message_factory ,sample_repo_i
     produce_messages(RESOURCE_TOPIC,  kafka_message)
     logger.info("Test messages produced to input topic.")
 
+    try:
+        processed_messages = consume_messages(RESOURCE_TOPIC, num_messages=1)
+        logger.info(f"Consumed {len(processed_messages)} messages from output topic.")
+        assert len(processed_messages) > 0, "No messages consumed from topic"
+    except TimeoutError as e:
+        logger.error(e)
+        assert False, str(e)
+
     def verify_message_structure(messages):
         assert len(messages) > 0, "No messages consumed from topic"
         for msg in messages:
