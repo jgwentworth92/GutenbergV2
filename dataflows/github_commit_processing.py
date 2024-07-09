187d1360561f76b0f41fd9fddd9da298c1cdfa3b
jgwentworth92 <alphabetboi92@protonmail.com>
2024-07-09 15:11:25 -0400

    removed batching and added statis update functs

--- dataflows/github_commit_processing.py
+++ dataflows/github_commit_processing.py
@@ -1,17 +1,21 @@
 from datetime import timedelta
 from bytewax.dataflow import Dataflow
 import bytewax.operators as op
-from bytewax.connectors.kafka import KafkaSource, KafkaSink, KafkaSinkMessage
+from bytewax.connectors.kafka import KafkaSource, KafkaSink, KafkaSinkMessage, KafkaSourceMessage
 from icecream import ic
 from confluent_kafka import OFFSET_STORED
 from config.config_setting import config
-from logging_config import setup_logging
+from logging_config import setup_logging, get_logger
+from models import constants
 from services.github_service import fetch_and_emit_commits
 import orjson
 
-from utils.dataflow_processing_utils import  extract_job_id
+from services.user_management_service import user_management_service
+from utils.dataflow_processing_utils import extract_job_id
 
 setup_logging()
+logger = get_logger(__name__)
+
 # Application setup
 brokers = [config.BROKERS]
 input_topic = config.GITHUB_TOPIC
@@ -28,25 +32,45 @@ kafka_input = op.input("kafka-in", flow,
                        KafkaSource(brokers=brokers, topics=[input_topic],
                                    add_config=producer_config))
 
-# Fetch and emit each commit individually
-processed_commits = op.flat_map("fetch_and_emit_commits", kafka_input,
-                                lambda msg: fetch_and_emit_commits(orjson.loads(msg.value)))
+
+def update_status_in_progress(msg: KafkaSourceMessage):
+    message = orjson.loads(msg.value)
+
+    job_id = message["job_id"]
+    logger.info(f"Updating status for job id {job_id}")
+    user_management_service.update_status(
+        job_id,
+        constants.Service.DATAFLOW_TYPE_processing_raw.value,
+        constants.StepStatus.IN_PROGRESS.value,
+    )
+    return  message
 
 
-# Key the stream by 'job_id'
-keyed_docs = op.key_on("key_by_job_id", processed_commits, extract_job_id)
+messages = op.map(
+    "update_status_in_progress",
+    kafka_input,
+    update_status_in_progress,
+)
 
-# Collect batches of records keyed by the same job_id
-batched_docs = op.collect("collect_docs", keyed_docs, timeout=timedelta(seconds=1), max_size=50)
+processed_commits = op.flat_map("fetch_and_emit_commits", messages,
+                                lambda msg: fetch_and_emit_commits(msg))
 
-# Remove the job_id key
-keyless_docs = op.map("remove_key", batched_docs, lambda x: x[1])
 
-# Create KafkaSinkMessages for each serialized commit data
-kafka_messages = op.map("create_kafka_messages", keyless_docs, lambda x: KafkaSinkMessage(None, orjson.dumps(x)))
+def update_status_complete(processed_docs):
+    if processed_docs:
+        job_id = processed_docs[0].metadata["job_id"]
+        logger.info(f"Updating status to complete for job id {job_id}")
+        user_management_service.update_status(
+            job_id,
+            constants.Service.DATAFLOW_TYPE_processing_raw.value,
+            constants.StepStatus.COMPLETE.value,
+        )
+    return [doc.model_dump_json() for doc in processed_docs]
+
+
+completed_messages = op.map("update_status_complete", processed_commits, update_status_complete)
+kafka_messages = op.map("create_kafka_messages", completed_messages, lambda x: KafkaSinkMessage(None, orjson.dumps(x)))
 
 # Output serialized data to Kafka
 op.output("kafka-vector-raw-add", kafka_messages,
-          KafkaSink(brokers=brokers, topic=output_topic, add_config=producer_config, ))  # to vector db
-op.output("kafka-output", kafka_messages,
-          KafkaSink(brokers=brokers, topic=config.PROCESSED_TOPIC, add_config=producer_config, ))  # to local llm
+          KafkaSink(brokers=brokers, topic=output_topic, add_config=producer_config, ))
