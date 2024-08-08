ce264657bd89cce15afa700cd96f9468992d0921
jgwentworth92 <alphabetboi92@protonmail.com>
2024-08-08 12:26:46 -0400

    refac service to use user  management wrapper class and standardized kafka message.

--- dataflows/commit_summary_service.py
+++ dataflows/commit_summary_service.py
@@ -1,18 +1,18 @@
-import json
-from datetime import timedelta
 from bytewax.dataflow import Dataflow
 import bytewax.operators as op
-from bytewax.connectors.kafka import operators as kop, KafkaSource, KafkaSink, KafkaSinkMessage, KafkaSourceMessage
-from icecream import ic
-from config.config_setting import config
+import bytewax.operators as op
 import orjson
+from bytewax.connectors.kafka import KafkaSource, KafkaSink, KafkaSinkMessage, KafkaSourceMessage
+from bytewax.dataflow import Dataflow
 from confluent_kafka import OFFSET_STORED
+from icecream import ic
+
+from config.config_setting import config
+from dataflows.github_commit_processing import serialize_standardized_message
 from logging_config import setup_logging, get_logger
 from models import constants
-from models.document import Document
-from services.user_management_service import user_management_service
 from services.message_processing_service import process_messages
-from utils.dataflow_processing_utils import extract_job_id
+from utils.status_update import StandardizedMessage, create_status_updater
 
 setup_logging()
 # Application setup
@@ -27,51 +27,38 @@ producer_config = config.PRODUCER_CONFIG
 flow = Dataflow("commit_summary_service")
 ic(f"the stored offset is {OFFSET_STORED}")
 
-# Create KafkaSource for consuming messages from Kafka
-kafka_input = kop.input("kafka-in-2", flow, brokers=brokers, topics=[input_topic])
-op.inspect("inspect_err", kafka_input.errs).then(op.raises, "raise_errors")
 
-
-# Collect batches of records keyed by the same job_id
-def update_status_in_progress(msg: KafkaSourceMessage):
-    messages = orjson.loads(msg.value)
-    data = [Document.model_validate_json(doc_json) for doc_json in messages]
-
-    row = data[0].metadata
-
-    job_id = row["job_id"]
-    logger.info(f"Updating status for job id {job_id}")
-    user_management_service.update_status(
-        job_id,
-        constants.Service.DATAFLOW_TYPE_processing_llm.value,
-        constants.StepStatus.IN_PROGRESS.value,
+def kafka_to_standardized(msg: KafkaSourceMessage) -> StandardizedMessage:
+    data = orjson.loads(msg.value)
+    return StandardizedMessage(
+        job_id=data["job_id"],
+        step_number=3,  # Assuming this is the second step in the overall process
+        data=data,
+        metadata={"original_topic": msg.topic}
     )
-    return data  # Return the validated documents instead of the original message
 
 
-# Process each message
-messages = op.map(
-    "update_status_in_progress",
-    kafka_input.oks,
-    update_status_in_progress,
+# Create KafkaSource for consuming messages from Kafka
+kafka_input = op.input("kafka-in", flow,
+                       KafkaSource(brokers=brokers, topics=[input_topic], add_config=consumer_config))
+
+standardized_messages = op.map(
+    "kafka_to_standardized",
+    kafka_input,
+    kafka_to_standardized,
 )
-processed_messages = op.flat_map("process_message", messages,
-                                 lambda docs: process_messages(docs))
+status_updater = create_status_updater(constants.Service.DATAFLOW_TYPE_processing_llm)
+
+processed_messages = op.flat_map("process_message", standardized_messages,
+                                 status_updater(process_messages))
 
 
-def update_status_complete(processed_docs):
-    if processed_docs:
-        job_id = processed_docs[0].metadata["job_id"]
-        logger.info(f"Updating status to complete for job id {job_id}")
-        user_management_service.update_status(
-            job_id,
-            constants.Service.DATAFLOW_TYPE_processing_llm.value,
-            constants.StepStatus.COMPLETE.value,
-        )
+def document_dump(processed_docs):
     return [doc.model_dump_json() for doc in processed_docs]  # Convert each Document to JSON string
 
 
-completed_messages = op.map("update_status_complete", processed_messages, update_status_complete)
-kafka_messages = op.map("create_kafka_messages", completed_messages, lambda x: KafkaSinkMessage(None, orjson.dumps(x)))
+serialized_docs = op.map("serialize_documents", processed_messages , serialize_standardized_message)
+
+kafka_messages = op.map("create_kafka_messages", serialized_docs, lambda x: KafkaSinkMessage(None, x))
 
 op.output("kafka-output", kafka_messages, KafkaSink(brokers=brokers, topic=output_topic))
