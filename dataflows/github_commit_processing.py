90cb000b70975aa49d814a1efabf710e2427b860
jgwentworth92 <alphabetboi92@protonmail.com>
2024-08-08 12:26:57 -0400

    refac service to use user  management wrapper class and standardized kafka message.

--- dataflows/github_commit_processing.py
+++ dataflows/github_commit_processing.py
@@ -1,17 +1,15 @@
-from datetime import timedelta
-from bytewax.dataflow import Dataflow
 import bytewax.operators as op
+import orjson
 from bytewax.connectors.kafka import KafkaSource, KafkaSink, KafkaSinkMessage, KafkaSourceMessage
-from icecream import ic
+from bytewax.dataflow import Dataflow
 from confluent_kafka import OFFSET_STORED
+from icecream import ic
+
 from config.config_setting import config
 from logging_config import setup_logging, get_logger
 from models import constants
 from services.github_service import fetch_and_emit_commits
-import orjson
-
-from services.user_management_service import user_management_service
-from utils.dataflow_processing_utils import extract_job_id
+from utils.status_update import create_status_updater, StandardizedMessage
 
 setup_logging()
 logger = get_logger(__name__)
@@ -33,44 +31,41 @@ kafka_input = op.input("kafka-in", flow,
                                    add_config=producer_config))
 
 
-def update_status_in_progress(msg: KafkaSourceMessage):
-    message = orjson.loads(msg.value)
-
-    job_id = message["job_id"]
-    logger.info(f"Updating status for job id {job_id}")
-    user_management_service.update_status(
-        job_id,
-        constants.Service.DATAFLOW_TYPE_processing_raw.value,
-        constants.StepStatus.IN_PROGRESS.value,
+def kafka_to_standardized(msg: KafkaSourceMessage) -> StandardizedMessage:
+    data = orjson.loads(msg.value)
+    return StandardizedMessage(
+        job_id=data["job_id"],
+        step_number=2,  # Assuming this is the second step in the overall process
+        data=data,
+        metadata={"original_topic": msg.topic}
     )
-    return  message
 
 
-messages = op.map(
-    "update_status_in_progress",
+standardized_messages = op.map(
+    "kafka_to_standardized",
     kafka_input,
-    update_status_in_progress,
+    kafka_to_standardized,
 )
 
-processed_commits = op.flat_map("fetch_and_emit_commits", messages,
-                                lambda msg: fetch_and_emit_commits(msg))
+# Create status updater for this service
+status_updater = create_status_updater(constants.Service.DATAFLOW_TYPE_processing_raw)
+
+# Wrap fetch_and_emit_commits with status updater
+processed_commits = op.flat_map(
+    "fetch_and_emit_commits_with_status",
+    standardized_messages,
+    status_updater(fetch_and_emit_commits)
+)
 
 
-def update_status_complete(processed_docs):
-    if processed_docs:
-        job_id = processed_docs[0].metadata["job_id"]
-        logger.info(f"Updating status to complete for job id {job_id}")
-        user_management_service.update_status(
-            job_id,
-            constants.Service.DATAFLOW_TYPE_processing_raw.value,
-            constants.StepStatus.COMPLETE.value,
-        )
-    return [doc.model_dump_json() for doc in processed_docs]
+def serialize_standardized_message(msg: StandardizedMessage):
+    return orjson.dumps(msg.__dict__)
 
 
-completed_messages = op.map("update_status_complete", processed_commits, update_status_complete)
-kafka_messages = op.map("create_kafka_messages", completed_messages, lambda x: KafkaSinkMessage(None, orjson.dumps(x)))
+serialized_docs = op.map("serialize_documents", processed_commits, serialize_standardized_message)
+
+kafka_messages = op.map("create_kafka_messages", serialized_docs, lambda x: KafkaSinkMessage(None, x))
 
 # Output serialized data to Kafka
 op.output("kafka-vector-raw-add", kafka_messages,
-          KafkaSink(brokers=brokers, topic=output_topic, add_config=producer_config, ))
+          KafkaSink(brokers=brokers, topic=output_topic, add_config=producer_config))
